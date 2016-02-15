5020d72c11df0ef6199148c93ec24c751b59b74a
Develop Mix <developmix@gmail.com>
2016-02-15 20:08:58 +0800

    Created Fragment page and DrawbleMenu

--- ViasApp/app/src/main/java/ru/vias/viasapp/MainActivity.java
+++ ViasApp/app/src/main/java/ru/vias/viasapp/MainActivity.java
@@ -1,13 +1,51 @@
 package ru.vias.viasapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int LAYOUT = R.layout.activity_main;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
+
+        initToolbar();
+        initNavigationView();
     }
+
+    private void initNavigationView() {
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new MainFragment());
+        fragmentTransaction.commit();
+
+        toggle.syncState();
+
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+    }
+
+
 }
